173a0ab0d36718d66f2cc69306addbfb6e7eb968
Lucas de Souza <lucas@pop-os.localdomain>
2022-06-14 18:08:34 -0300

    feat: Estilizando o texto logo do app

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,39 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            const Text(
-              'CorreAqui',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+        body: Column(children: [
+          RichText(
+            text: const TextSpan(
+                text: 'C',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                    fontSize: 28),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'orre',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white70,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'A',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        fontSize: 28),
+                  ),
+                  TextSpan(
+                    text: 'qui',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white70,
+                    ),
+                  ),
+                ]),
+          )
+        ]),
       ),
     );
   }

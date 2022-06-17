cee3c985e2b4dfb1d6a018b6c4db2096bf7f2dc2
lucasnomi <lucas04.dev@gmail.com>
2022-06-17 20:07:04 -0300

    feat: construido layout da pagina de cadastro

---
+++ lib/routes/sign_in.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.person_add),
+                  label: const Text('Cadastrar com email')),
+              ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.person_add),
+                  label: const Text('Cadastrar com google')),
+            ],
+          ),
+        ));
+  }
+}

4ef7ddc4071254a5442d5c39aa22dce5a6349e91
lucasnomi <lucas04.dev@gmail.com>
2022-06-17 15:50:03 -0300

    feat: adicionar responsive framework e a homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:servicos_batatais/widgets/homepage.dart';
+import 'package:servicos_batatais/routes/homepage.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
-void main() {
-  runApp(const MainPage());
-}
+void main() => runApp(const MainPage());
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -11,6 +10,20 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) => ResponsiveWrapper.builder(
+        child,
+        defaultScale: true,
+        breakpoints: const [
+          ResponsiveBreakpoint.resize(350, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(600, name: TABLET),
+          ResponsiveBreakpoint.resize(800, name: DESKTOP),
+          ResponsiveBreakpoint.autoScale(1000, name: 'XL')
+        ],
+        background: Container(
+          color: const Color.fromARGB(255, 0, 0, 0),
+        ),
+      ),
+      initialRoute: '/',
       home: const HomePage(),
     );
   }

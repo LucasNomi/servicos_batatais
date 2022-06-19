bcbb97200e7967a9f0d60bfdcc35d3a609fecdd6
lucasnomi <lucas04.dev@gmail.com>
2022-06-19 14:45:00 -0300

    fix: pagina home como pagina de cadastro

--- lib/routes/homepage.dart
+++ lib/routes/homepage.dart
@@ -13,27 +13,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => const SignIn()));
-                },
-                icon: const Icon(Icons.person_add),
-                label: const Text("Cadastrar")),
-            ElevatedButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.login),
-              label: const Text("Logar"),
-            )
-          ],
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }

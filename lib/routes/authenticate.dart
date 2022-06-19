cc75fad6d29c779c3eb5e41e5b930a90e8d0b396
lucasnomi <lucas04.dev@gmail.com>
2022-06-19 14:43:33 -0300

    feat: criado authenticate para selecionar sign_up ou sign_in no app

---
+++ lib/routes/authenticate.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:servicos_batatais/routes/sign_in.dart';
+
+class Authenticate extends StatelessWidget {
+  const Authenticate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.person_add),
+                label: const Text("Cadastrar")),
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const SignIn()));
+              },
+              icon: const Icon(Icons.login),
+              label: const Text("Logar"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

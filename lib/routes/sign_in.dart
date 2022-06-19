7159fe56e88103861557eec3afe2cc483e02e675
lucasnomi <lucas04.dev@gmail.com>
2022-06-19 14:44:31 -0300

    fix: pagina de login como pagina de cadastro

--- lib/routes/sign_in.dart
+++ lib/routes/sign_in.dart
@@ -11,21 +11,22 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.login),
-                  label: const Text('ENTRAR COM O EMAIL')),
-              ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.login),
-                  label: const Text('ENTRAR COM O GOOGLE')),
-            ],
-          ),
-        ));
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.login),
+                label: const Text('ENTRAR COM O EMAIL')),
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.login),
+                label: const Text('ENTRAR COM O GOOGLE')),
+          ],
+        ),
+      ),
+    );
   }
 }

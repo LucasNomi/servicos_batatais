9b1b685bf60691ebc06673608659d9f6cd8d9d22
lucasnomi <lucas04.dev@gmail.com>
2022-06-21 20:05:23 -0300

    feat: centralizar o formulario

--- lib/routes/sign_up.dart
+++ lib/routes/sign_up.dart
@@ -23,6 +23,7 @@ class _SignUpState extends State<SignUp> {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 50.0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextFormField(
                   controller: _emailController,

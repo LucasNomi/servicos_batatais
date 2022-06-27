38bc09c57ac6075a41c66ea770a7229592cbd8b4
lucasnomi <lucas04.dev@gmail.com>
2022-06-27 17:49:56 -0300

    feat: adicionado um provider para o AuthService

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_framework.dart';
+import 'package:provider/provider.dart';
 import 'package:servicos_batatais/routes/wrapper.dart';
+import 'package:servicos_batatais/services/auth_service.dart';
 
 void main() async {
   await Firebase.initializeApp();
 
-  runApp(const MainPage());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: ((context) => AuthService())),
+    ],
+    child: const MainPage(),
+  ));
 }
 
 class MainPage extends StatelessWidget {

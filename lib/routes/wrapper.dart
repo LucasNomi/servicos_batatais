3ce6dee3c4ae15643d75404b8d5fb8036a5eb1a3
lucasnomi <lucas04.dev@gmail.com>
2022-06-27 17:50:51 -0300

    feat: configurado os casos de verificacao para usuario logado ou nao

--- lib/routes/wrapper.dart
+++ lib/routes/wrapper.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:servicos_batatais/routes/authenticate.dart';
+import 'package:servicos_batatais/routes/homepage.dart';
+import 'package:servicos_batatais/services/auth_service.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Authenticate();
+    AuthService auth = Provider.of<AuthService>(context);
+    if (auth.isLoading) {
+      return Loading();
+    } else if (auth.usuario == null) {
+      return const Authenticate();
+    } else {
+      return const HomePage();
+    }
+  }
+
+  Loading() {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 }

f89d73cf366b9a9f04ef68794848abbaee92207e
lucasnomi <lucas04.dev@gmail.com>
2022-06-27 17:50:22 -0300

    feat: criado os codigos para verificar se o usuario esta logado

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,3 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 
-class AuthService extends ChangeNotifier {}
+class AuthService extends ChangeNotifier {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  User? usuario;
+  bool isLoading = true;
+
+  AuthService() {
+    _authCheck();
+  }
+
+  _authCheck() {
+    _auth.authStateChanges().listen((User? user) {
+      usuario = (user == null) ? null : user;
+      isLoading = false;
+      notifyListeners();
+    });
+  }
+}

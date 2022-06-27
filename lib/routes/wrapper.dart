d1fcde79a7655d2b362843d3dd4cf0c54fb74ebd
lucasnomi <lucas04.dev@gmail.com>
2022-06-27 17:53:40 -0300

    fix: wrapper estava como stateless no lugar de stateful

--- lib/routes/wrapper.dart
+++ lib/routes/wrapper.dart
@@ -4,9 +4,14 @@ import 'package:servicos_batatais/routes/authenticate.dart';
 import 'package:servicos_batatais/routes/homepage.dart';
 import 'package:servicos_batatais/services/auth_service.dart';
 
-class Wrapper extends StatelessWidget {
+class Wrapper extends StatefulWidget {
   const Wrapper({Key? key}) : super(key: key);
 
+  @override
+  State<Wrapper> createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
   @override
   Widget build(BuildContext context) {
     AuthService auth = Provider.of<AuthService>(context);

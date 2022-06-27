bb2f65f02bf3330ffce5f3c0d6d52598a82b416b
lucasnomi <lucas04.dev@gmail.com>
2022-06-27 17:34:07 -0300

    feat: esperar pela inicializacao do firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'package:servicos_batatais/routes/wrapper.dart';
 
-void main() => runApp(const MainPage());
+void main() async {
+  await Firebase.initializeApp();
+
+  runApp(const MainPage());
+}
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);

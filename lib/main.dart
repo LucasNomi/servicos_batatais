2e22d1141ff0ce381a4c024528b6187daa97c361
lucasnomi <lucas04.dev@gmail.com>
2022-07-03 15:44:01 -0300

    feat: refeito o codigo para iniciar o app, separando o MyApp do main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:responsive_framework/responsive_framework.dart';
 import 'package:provider/provider.dart';
+import 'package:servicos_batatais/my_app.dart';
 import 'package:servicos_batatais/firebase_options.dart';
-import 'package:servicos_batatais/routes/wrapper.dart';
 import 'package:servicos_batatais/services/auth_service.dart';
 
 void main() async {
@@ -15,25 +14,6 @@ void main() async {
     providers: [
       ChangeNotifierProvider(create: ((context) => AuthService())),
     ],
-    child: const MainPage(),
+    child: const MyApp(),
   ));
-}
-
-class MainPage extends StatelessWidget {
-  const MainPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      builder: (context, child) => ResponsiveWrapper.builder(
-        child,
-        defaultScale: true,
-        breakpoints: const [
-          ResponsiveBreakpoint.resize(480, name: MOBILE),
-          ResponsiveBreakpoint.autoScale(780, name: TABLET),
-        ],
-      ),
-      home: const Wrapper(),
-    );
-  }
 }

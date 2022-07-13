99ba4845d11a68c2a111fe79df44cea67ce7598e
lucasnomi <lucas04.dev@gmail.com>
2022-07-13 18:10:21 -0300

    feat: mudar o tipo da widget de authenticate de stls para stfl

--- lib/routes/authenticate.dart
+++ lib/routes/authenticate.dart
@@ -1,24 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:servicos_batatais/widgets/logo.dart';
 
-class Authenticate extends StatelessWidget {
+class Authenticate extends StatefulWidget {
   const Authenticate({Key? key}) : super(key: key);
 
   @override
+  State<Authenticate> createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(130, 195, 195, 195),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const LogoCorreAqui(),
-            const SizedBox(
-              height: 50.0,
-            ),
-          ],
-        ),
-      ),
-    );
+    return Container();
   }
 }

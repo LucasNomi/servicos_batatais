0d27960dac7ded0cf997f30163d0cf0799cef2fb
lucasnomi <lucas04.dev@gmail.com>
2022-07-15 19:55:03 -0300

    feat: efetuar a troca do auth quando clicado no botao

--- lib/routes/authenticate.dart
+++ lib/routes/authenticate.dart
@@ -17,74 +17,82 @@ class _AuthenticateState extends State<Authenticate> {
   late Text btnChangeAuth;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    bool isSignIn = true;
-    btnLRLabel = Text('Login');
-    iconLR = Icon(Icons.login);
-    btnChangeAuth = Text('Ainda não tem uma conta? Registre-se!');
-
-    if(isSignIn == false){
-      btnLRLabel = Text('Registrar');
-      iconLR = Icon(Icons.person_add);
-      btnChangeAuth = Text('Já possuí uma conta? Efetue o login!');
-    }
+    setFormAction(true);
   }
 
+  setFormAction(bool acao) {
+    setState(() {
+      isSignIn = acao;
+      if (isSignIn == true) {
+        btnLRLabel = Text('Login');
+        iconLR = Icon(Icons.login);
+        btnChangeAuth = Text('Ainda não possuí uma conta? Cadastre-se!');
+      } else {
+        btnLRLabel = Text('Cadastrar');
+        iconLR = Icon(Icons.person_add);
+        btnChangeAuth = Text('Já possuí uma conta? Efetue o login!');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              controller: _email,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-              validator: (String? val) {
-                if(val == null || val.isEmpty){
-                  return 'Por favor insira um email';
-                }else if(!val.contains('@')){
-                  return 'Por favor insira um email valido';
-                }
-                return null;
+    return Scaffold(
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _email,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? val) {
+                  if (val == null || val.isEmpty) {
+                    return 'Por favor insira um email';
+                  } else if (!val.contains('@')) {
+                    return 'Por favor insira um email valido';
+                  }
+                  return null;
                 },
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            TextFormField(
-              controller: _password,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
               ),
-              validator: (String? val){
-                if(val == null || val.isEmpty){
-                  return 'Por favor insira uma senha';
-                }else if(val.length < 8){
-                  return 'Senha precisa ter mais que 8 digitos';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            ElevatedButton.icon(onPressed: (){}, icon: iconLR, label: btnLRLabel),
-            const SizedBox(
-              height: 50.0,
-            ),
-            TextButton(onPressed: (){
-              isSignIn == false;
-            }, child: btnChangeAuth),
-          ],
+              const SizedBox(
+                height: 30.0,
+              ),
+              TextFormField(
+                controller: _password,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? val) {
+                  if (val == null || val.isEmpty) {
+                    return 'Por favor insira uma senha';
+                  } else if (val.length < 8) {
+                    return 'Senha precisa ter mais que 8 digitos';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              ElevatedButton.icon(
+                  onPressed: () {}, icon: iconLR, label: btnLRLabel),
+              const SizedBox(
+                height: 50.0,
+              ),
+              TextButton(
+                  onPressed: () => setFormAction(!isSignIn),
+                  child: btnChangeAuth),
+            ],
+          ),
         ),
       ),
     );

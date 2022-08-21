03e1fdf7bfe915a89cd981cb0acb048d3495816d
LucasNomi <lucas04.dev@gmail.com>
2022-08-21 19:28:45 -0300

    feat: conexao com o firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:servicos_batatais/firebase_options.dart';
 import 'package:servicos_batatais/responsive/mobile_screen_layout.dart';
 import 'package:servicos_batatais/responsive/responsive_layout.dart';
 import 'package:servicos_batatais/responsive/web_screen_layout.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 

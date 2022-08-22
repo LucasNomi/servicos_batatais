b88b30848e4c6799c685f0deb485176dc1637f3b
LucasNomi <lucas04.dev@gmail.com>
2022-08-21 23:30:21 -0300

    feat: cor nova de fundo

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
 const primaryColor = Color.fromRGBO(35, 53, 224, 88);
 const secondaryColor = Color.fromRGBO(67, 130, 250, 98);
 const hightLightColor = Colors.blue;

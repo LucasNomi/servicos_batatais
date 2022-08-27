32ba4bc3060697aa1f413503045e3a4ebc185eb9
LucasNomi <lucas04.dev@gmail.com>
2022-08-27 13:39:08 -0300

    feat: validator methods

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -12,3 +12,41 @@ pickImage(ImageSource source) async {
     print("No image Selected");
   }
 }
+
+String? validateUsername(String? formUsername) {
+  if (formUsername == null || formUsername.isEmpty) {
+    return 'Um nome de usuário é necessário';
+  }
+  return null;
+}
+
+String? validateEmail(String? formEmail) {
+  if (formEmail == null || formEmail.isEmpty) {
+    return 'Um e-mail é necessário';
+  }
+
+  String pattern = r'\w+@\w+\.\w+';
+  RegExp regex = RegExp(pattern);
+  if (!regex.hasMatch(formEmail)) {
+    return 'O e-mail é inválido';
+  }
+
+  return null;
+}
+
+String? validatePassword(String? formPassword) {
+  if (formPassword == null || formPassword.isEmpty) {
+    return 'Uma senha é necessária';
+  }
+
+  String pattern =
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{6,}$';
+  RegExp regex = RegExp(pattern);
+  if (!regex.hasMatch(formPassword)) {
+    return '''
+      Senha precisa ser de no mínimo 6 caracteres,
+      Inclua: Uma letra maiúscula, número e símbolo.
+      ''';
+  }
+  return null;
+}

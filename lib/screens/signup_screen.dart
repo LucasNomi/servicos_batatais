30518e8f90fc507440bf15e7265691ae39ef7200
LucasNomi <lucas04.dev@gmail.com>
2022-08-27 13:38:01 -0300

    feat: form validator, singlechildview

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -5,7 +5,7 @@ import 'package:servicos_batatais/services/auth_service.dart';
 import 'package:servicos_batatais/utils/utils.dart';
 
 import '../utils/colors.dart';
-import '../widgets/text_field_input.dart';
+import '../widgets/text__form_field_input.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -15,10 +15,12 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final _formKey = GlobalKey<FormState>();
   final _email = TextEditingController();
   final _password = TextEditingController();
   final _userName = TextEditingController();
   Uint8List? _image;
+  bool _isLoading = false;
 
   @override
   void dispose() {
@@ -39,141 +41,152 @@ class _SignUpScreenState extends State<SignUpScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 26),
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Flexible(
-                flex: 2,
-                child: Container(),
-              ),
-              //TODO: logo
-              const SizedBox(
-                height: 64,
-              ),
-              //* circular avatar input
-              Stack(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 26),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  _image != null
-                      ? CircleAvatar(
-                          radius: 64,
-                          backgroundColor: Colors.indigo[50],
-                          backgroundImage: MemoryImage(_image!),
-                        )
-                      : CircleAvatar(
-                          radius: 64,
-                          backgroundColor: Colors.indigo[50],
-                          backgroundImage: const NetworkImage(
-                              'https://www.pinterest.com/pin/710231803741582825/'),
+                  //TODO: logo
+                  const SizedBox(
+                    height: 64,
+                  ),
+                  //* circular avatar input
+                  Stack(
+                    children: [
+                      _image != null
+                          ? CircleAvatar(
+                              radius: 64,
+                              backgroundColor: Colors.indigo[50],
+                              backgroundImage: MemoryImage(_image!),
+                            )
+                          : CircleAvatar(
+                              radius: 64,
+                              backgroundColor: Colors.indigo[50],
+                              backgroundImage:
+                                  const AssetImage('assets/nopfp.png'),
+                            ),
+                      Positioned(
+                        bottom: -10,
+                        left: 80,
+                        child: IconButton(
+                          onPressed: selectImage,
+                          icon: const Icon(Icons.add_a_photo),
                         ),
-                  Positioned(
-                    bottom: -10,
-                    left: 80,
-                    child: IconButton(
-                      onPressed: selectImage,
-                      icon: const Icon(Icons.add_a_photo),
-                    ),
-                  ),
-                ],
-              ),
-              //* username text field
-              const SizedBox(
-                height: 30,
-              ),
-              TextFieldInput(
-                controller: _userName,
-                icon: const Icon(Icons.person),
-                hintText: 'Seu nome de usuário aqui',
-                inputType: TextInputType.text,
-                labelText: 'Usuário',
-              ),
-              //* text field email
-              const SizedBox(
-                height: 12,
-              ),
-              TextFieldInput(
-                controller: _email,
-                icon: const Icon(Icons.email),
-                hintText: 'Seu email aqui',
-                inputType: TextInputType.emailAddress,
-                labelText: 'Email',
-              ),
-              //* text field password
-              const SizedBox(
-                height: 12,
-              ),
-              TextFieldInput(
-                  controller: _password,
-                  icon: const Icon(Icons.key),
-                  labelText: 'Senha',
-                  hintText: 'Sua senha aqui',
-                  inputType: TextInputType.text,
-                  obscureText: true),
-              //TODO: button login
-              const SizedBox(
-                height: 12,
-              ),
-              InkWell(
-                onTap: () async {
-                  String res = await AuthService().signUpUser(
-                      email: _email.text,
-                      password: _password.text,
-                      username: _userName.text,
-                      imageUrl: _image!);
-                  print(res);
-                },
-                child: Container(
-                  width: double.infinity,
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.symmetric(vertical: 12),
-                  decoration: const ShapeDecoration(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(5),
                       ),
-                    ),
-                    color: primaryColor,
+                    ],
                   ),
-                  child: const Text(
-                    'Cadastrar',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
-                    ),
+                  //* username text field
+                  const SizedBox(
+                    height: 30,
                   ),
-                ),
-              ),
-              //TODO: transition to signup
-              Flexible(
-                flex: 2,
-                child: Container(),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: const Text('Já possui uma conta?'),
+                  TextFormFieldInput(
+                    controller: _userName,
+                    icon: const Icon(Icons.person),
+                    hintText: 'Seu nome de usuário aqui',
+                    inputType: TextInputType.text,
+                    labelText: 'Usuário',
+                    validator: validateUsername,
                   ),
-                  GestureDetector(
-                    onTap: () {},
+                  //* text field email
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  TextFormFieldInput(
+                    controller: _email,
+                    icon: const Icon(Icons.email),
+                    hintText: 'Seu email aqui',
+                    inputType: TextInputType.emailAddress,
+                    labelText: 'Email',
+                    validator: validateEmail,
+                  ),
+                  //* text field password
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  TextFormFieldInput(
+                    controller: _password,
+                    icon: const Icon(Icons.key),
+                    labelText: 'Senha',
+                    hintText: 'Sua senha aqui',
+                    inputType: TextInputType.text,
+                    obscureText: true,
+                    validator: validatePassword,
+                  ),
+                  //* button signup
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        _isLoading = true;
+                      });
+                      if (_formKey.currentState!.validate()) {
+                        await AuthService().signUpUser(
+                            email: _email.text,
+                            password: _password.text,
+                            username: _userName.text,
+                            imageUrl: _image!);
+                      }
+
+                      setState(() {
+                        _isLoading = false;
+                      });
+                    },
                     child: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: const Text(
-                        ' Efetue o login',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: hightLightColor,
-                          fontSize: 14,
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      decoration: const ShapeDecoration(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
                         ),
+                        color: primaryColor,
                       ),
+                      child: _isLoading
+                          ? const Center(child: CircularProgressIndicator())
+                          : const Text(
+                              'Cadastrar',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
                     ),
+                  ),
+                  //TODO: transition to signup
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: const Text('Já possui uma conta?'),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: const Text(
+                            ' Efetue o login',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: hightLightColor,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
         ),
       ),

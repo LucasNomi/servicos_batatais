d6f8443e70303d63d61ea98e033335f83e4dc43d
LucasNomi <lucas04.dev@gmail.com>
2022-08-27 16:01:22 -0300

    feat: login com email e senha e add validator

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:servicos_batatais/screens/signup_screen.dart';
+import 'package:servicos_batatais/services/auth_service.dart';
 import 'package:servicos_batatais/utils/colors.dart';
+import 'package:servicos_batatais/utils/utils.dart';
 
 import '../widgets/text_form_field_input.dart';
 
@@ -11,8 +14,10 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
   final _email = TextEditingController();
   final _password = TextEditingController();
+  final bool _isLoading = false;
 
   @override
   void dispose() {
@@ -22,99 +27,111 @@ class _LoginScreenState extends State<LoginScreen> {
     _password.dispose();
   }
 
+  loginUser() async {
+    try {
+      if (_formKey.currentState!.validate()) {
+        await AuthService()
+            .loginUser(email: _email.text, password: _password.text);
+      }
+    } on AuthException catch (e) {
+      showSnackBar(e.message, context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 26),
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Flexible(
-                flex: 2,
-                child: Container(),
-              ),
-              //TODO: logo
-              //* text field email
-              const SizedBox(
-                height: 64,
-              ),
-              TextFormFieldInput(
-                controller: _email,
-                icon: const Icon(Icons.email),
-                hintText: 'Seu email aqui',
-                inputType: TextInputType.emailAddress,
-                labelText: 'Email',
-              ),
-              //* text field password
-              const SizedBox(
-                height: 12,
-              ),
-              TextFormFieldInput(
-                  controller: _password,
-                  icon: const Icon(Icons.key),
-                  labelText: 'Senha',
-                  hintText: 'Sua senha aqui',
-                  inputType: TextInputType.text,
-                  obscureText: true),
-              //TODO: button login
-              const SizedBox(
-                height: 12,
-              ),
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  width: double.infinity,
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.symmetric(vertical: 12),
-                  decoration: const ShapeDecoration(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(5),
-                      ),
-                    ),
-                    color: primaryColor,
-                  ),
-                  child: const Text(
-                    'Entrar',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-              ),
-              //TODO: transition to signup
-              Flexible(
-                flex: 2,
-                child: Container(),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 26),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: const Text('Ainda não possui uma conta?'),
+                  //TODO: logo
+                  //* text field email
+                  const SizedBox(
+                    height: 64,
                   ),
-                  GestureDetector(
-                    onTap: () {},
+                  TextFormFieldInput(
+                    controller: _email,
+                    icon: const Icon(Icons.email),
+                    hintText: 'Seu email aqui',
+                    inputType: TextInputType.emailAddress,
+                    labelText: 'Email',
+                    validator: validateEmail,
+                  ),
+                  //* text field password
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  TextFormFieldInput(
+                    controller: _password,
+                    icon: const Icon(Icons.key),
+                    labelText: 'Senha',
+                    hintText: 'Sua senha aqui',
+                    inputType: TextInputType.text,
+                    obscureText: true,
+                  ),
+                  //TODO: button login
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  InkWell(
+                    onTap: loginUser,
                     child: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: const Text(
-                        ' Registre-se',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: hightLightColor,
-                          fontSize: 14,
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      decoration: const ShapeDecoration(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
                         ),
+                        color: primaryColor,
                       ),
+                      child: _isLoading
+                          ? const Center(child: CircularProgressIndicator())
+                          : const Text(
+                              'Entrar',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
                     ),
+                  ),
+                  //TODO: transition to signup
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: const Text('Ainda não possui uma conta?'),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: const Text(
+                            ' Registre-se',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: hightLightColor,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
         ),
       ),

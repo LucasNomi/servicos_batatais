12fd3e95765426dc416a535fb4d5249593dfbe9d
LucasNomi <lucas04.dev@gmail.com>
2022-08-27 16:50:53 -0300

    ref: melhor leitura

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -52,7 +52,10 @@ class AuthService {
   }
 
   //* login
-  loginUser({required String email, required String password}) async {
+  loginUser({
+    required String email,
+    required String password,
+  }) async {
     String res = 'Some error occurred';
     try {
       if (email.isNotEmpty || password.isNotEmpty) {

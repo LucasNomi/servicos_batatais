f5763f46cf129d933095ce0da81d3624e66343b2
LucasNomi <lucas04.dev@gmail.com>
2022-08-27 21:13:24 -0300

    feat: mudar para stful e adicionado provider

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
 
+  @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("This is Mobile"),
+        child: Text('Oi'),
       ),
     );
   }

20d41b328394c965ba7b5502b47d34eae833efad
LucasNomi <lucas04.dev@gmail.com>
2022-09-03 13:32:01 -0300

    feat: removido isPost

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -7,8 +7,7 @@ class StorageService {
   final FirebaseStorage _storage = FirebaseStorage.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<String> uploadImagetoStorage(
-      String childName, Uint8List file, bool isPost) async {
+  Future<String> uploadImagetoStorage(String childName, Uint8List file) async {
     Reference ref =
         _storage.ref().child(childName).child(_auth.currentUser!.uid);
 

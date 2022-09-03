e35b6c928c8aaa7f59a4157e78877b341df85c15
LucasNomi <lucas04.dev@gmail.com>
2022-09-03 19:42:19 -0300

    feat: mostrar jobs card

--- lib/screens/jobs_screen.dart
+++ lib/screens/jobs_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/job_card.dart';
 
 class JobsScreen extends StatefulWidget {
   const JobsScreen({Key? key}) : super(key: key);
@@ -10,12 +13,22 @@ class JobsScreen extends StatefulWidget {
 class _JobsScreenState extends State<JobsScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Center(
-          child: Text('OI'),
-        ),
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('jobs').snapshots(),
+        builder: ((context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) => JobCard(
+                    snap: snapshot.data!.docs[index].data(),
+                  ));
+        }),
       ),
     );
   }

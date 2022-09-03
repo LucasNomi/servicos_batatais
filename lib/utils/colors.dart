c3f02c443730ca4dc1297197b1f8be9277f84214
LucasNomi <lucas04.dev@gmail.com>
2022-09-03 11:38:51 -0300

    feat: adicionado nova cor highlight

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const mobileHighlightBackgroundColor = Color.fromARGB(255, 25, 25, 25);
 const primaryColor = Color.fromRGBO(35, 53, 224, 88);
 const secondaryColor = Color.fromRGBO(67, 130, 250, 98);
 const hightLightColor = Colors.blue;

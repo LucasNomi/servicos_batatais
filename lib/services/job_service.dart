295d5c83692011116ca883bf64c77484af7cd397
LucasNomi <lucas04.dev@gmail.com>
2022-09-03 13:30:47 -0300

    feat: funcoes para adicionar serviços

--- lib/services/job_service.dart
+++ lib/services/job_service.dart
@@ -1,3 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:uuid/uuid.dart';
 
-class JobService {}
+import '../models/jobs_model.dart';
+
+class JobService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+//* upload job to cloud firestore
+  uploadJob(
+    String uid,
+    String username,
+    String jobName,
+    String jobDesc,
+    String phoneNumber,
+  ) {
+    String res = 'Some error occurred';
+    try {
+      if (jobName.isNotEmpty || jobDesc.isNotEmpty || phoneNumber.isNotEmpty) {
+        String uuid = const Uuid().v1();
+        Jobs job = Jobs(
+            uid: uid,
+            username: username,
+            uuid: uuid,
+            jobName: jobName,
+            jobDesc: jobDesc,
+            phoneNumber: phoneNumber);
+
+        _firestore.collection('jobs').doc(uuid).set(job.toJson());
+        res = 'success';
+      }
+    } catch (e) {
+      res = e.toString();
+    }
+    return res;
+  }
+}

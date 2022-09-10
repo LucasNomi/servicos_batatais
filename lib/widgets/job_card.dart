79b2e7004e7235f6c4cf4fcf2d73e812f46a0176
LucasNomi <lucas04.dev@gmail.com>
2022-09-10 19:36:07 -0300

    feat: conexão com whatsapp

--- lib/widgets/job_card.dart
+++ lib/widgets/job_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 import '../utils/colors.dart';
 
@@ -6,47 +8,59 @@ class JobCard extends StatelessWidget {
   final snap;
   const JobCard({Key? key, required this.snap}) : super(key: key);
 
+  openWhatsapp() async {
+    String phoneNumber = snap['jobCountryCode'] + snap['phoneNumber'];
+    Uri whatsappUrl = Uri(scheme: "https", host: "wa.me", path: phoneNumber);
+
+    if (await canLaunchUrl(whatsappUrl)) {
+      await launchUrl(whatsappUrl);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: mobileHighlightBackgroundColor,
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      child: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16)
-                .copyWith(right: 0),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 20,
-                  backgroundImage: NetworkImage(snap['imageUrl']),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        snap['username'],
-                        style: const TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ],
+    return GestureDetector(
+      onTap: openWhatsapp,
+      child: Container(
+        color: mobileHighlightBackgroundColor,
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16)
+                  .copyWith(right: 0),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(snap['imageUrl']),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snap['username'],
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [Text(snap['jobName']), Text(snap['phoneNumber'])],
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [Text(snap['jobName']), Text(snap['phoneNumber'])],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
